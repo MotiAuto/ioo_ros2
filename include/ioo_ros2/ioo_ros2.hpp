527c0b107482a95a4ad10dc70fa80f406995ac91
motii8128 <default270robo@gmail.com>
2024-11-21 18:18:23 +0900

    Update

--- include/ioo_ros2/ioo_ros2.hpp
+++ include/ioo_ros2/ioo_ros2.hpp
@@ -2,6 +2,12 @@
 #define IOO_ROS2_HPP_
 
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <chrono>
+using namespace std::chrono_literals;
+using std::placeholders::_1;
 
 namespace ioo_ros2
 {
@@ -9,6 +15,16 @@ namespace ioo_ros2
     {
         public:
         explicit ImuOnlyOdometryROS2(const rclcpp::NodeOptions& option = rclcpp::NodeOptions());
+
+        void topic_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+        void timer_callback();
+
+        private:
+        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr subscriber_;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+
+        sensor_msgs::msg::Imu::SharedPtr get_msg;
     };
 }
 
--- src/ioo_ros2.cpp
+++ src/ioo_ros2.cpp
@@ -4,7 +4,20 @@ namespace ioo_ros2
 {
     ImuOnlyOdometryROS2::ImuOnlyOdometryROS2(const rclcpp::NodeOptions& option) : Node("ImuOnlyOdometry", option)
     {
+        rclcpp::QoS qos_settings = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+        subscriber_ = this->create_subscription<sensor_msgs::msg::Imu>(
+            "/imu",
+            qos_settings,
+            std::bind(&ImuOnlyOdometryROS2::topic_callback, this, _1));
 
+        publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 0);
+
+        timer_ = this->create_wall_timer(1ms, std::bind(&ImuOnlyOdometryROS2::timer_callback, this));
+    }
+
+    void ImuOnlyOdometryROS2::topic_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+    {
+        get_msg = msg;
     }
 }
 
