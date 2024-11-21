5cfed809949bdd77f44da5c2d55b12bcd3b8dad5
motii8128 <default270robo@gmail.com>
2024-11-21 10:10:19 +0900

    Update

--- include/ioo_ros2/posture.hpp
+++ include/ioo_ros2/posture.hpp
@@ -10,7 +10,7 @@ namespace ioo_ros2
         public:
         PostureEstimater();
 
-        Eigen::Vector3d estimate(Eigen::Vector3d input_matrix, Eigen::Vector3d linear_accel);
+        Eigen::Vector3d estimate(Eigen::Vector3d angular, Eigen::Vector3d linear_accel);
 
         private:
         Eigen::Matrix3d cov_;
@@ -26,11 +26,21 @@ namespace ioo_ros2
 
     Eigen::Matrix3d jacob(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation);
 
-    Eigen::Vector3d predict_x(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation);
+    Eigen::Vector3d predictX(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation);
 
-    Eigen::Matrix3d predict_cov(const Eigen::Matrix3d jacob, const Eigen::Matrix3d cov, const Eigen::Matrix3d est_noise);
+    Eigen::Matrix3d predictCov(const Eigen::Matrix3d jacob, const Eigen::Matrix3d cov, const Eigen::Matrix3d est_noise);
 
-    Eigen::Vector2d update_residual(const Eigen::Vector2d obs, const Eigen::Vector3d est);
+    Eigen::Vector2d updateResidual(const Eigen::Vector2d obs, const Eigen::Vector3d est);
+
+    Eigen::Matrix2d updateS(const Eigen::Matrix3d cov_, const Eigen::Matrix2d obs_noise);
+
+    Eigen::Matrix<double, 3, 2> updateKalmanGain(const Eigen::Matrix2d s, const Eigen::Matrix3d cov);
+
+    Eigen::Vector3d updateX(const Eigen::Vector3d est, const Eigen::Matrix<double, 3, 2> kalman_gain_, const Eigen::Vector2d residual);
+
+    Eigen::Matrix3d updateCov(const Eigen::Matrix<double, 3, 2> kalman_gain, const Eigen::Matrix3d cov);
+
+    Eigen::Vector2d obsModel(const Eigen::Vector3d linear_accel);
 }
 
 #endif
--- src/posture.cpp
+++ src/posture.cpp
@@ -18,6 +18,23 @@ namespace ioo_ros2
         estimation_noise_(2, 2) = 0.0174*0.001*0.001;
     }
 
+    Eigen::Vector3d PostureEstimater::estimate(Eigen::Vector3d angular, Eigen::Vector3d linear_accel)
+    {
+        auto input_matrix = getInputMatrix(angular.x(), angular.y(), angular.z());
+
+        auto jacob_ = jacob(input_matrix, estimation_);
+        estimation_ = predictX(input_matrix, estimation_);
+        cov_ = predictCov(jacob_, cov_, estimation_noise_);
+        auto obs_model = obsModel(linear_accel);
+        auto residual = updateResidual(obs_model, estimation_);
+        auto s = updateS(cov_, observation_noise_);
+        kalman_gain_ = updateKalmanGain(s, cov_);
+        estimation_ = updateX(estimation_, kalman_gain_, residual);
+        cov_ = updateCov(kalman_gain_, cov_);
+
+        return estimation_;
+    }
+
     Eigen::Vector3d getInputMatrix(double angular_x, double angular_y, double angular_z)
     {
         return Eigen::Vector3d(
@@ -62,7 +79,7 @@ namespace ioo_ros2
         return mat;
     }
 
-    Eigen::Vector3d predict_x(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation)
+    Eigen::Vector3d predictX(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation)
     {
         auto cos_roll = cos(estimation.x());
         auto sin_roll = sin(estimation.x());
@@ -77,7 +94,7 @@ namespace ioo_ros2
         return est;
     }
 
-    Eigen::Matrix3d predict_cov(const Eigen::Matrix3d jacob, const Eigen::Matrix3d cov, const Eigen::Matrix3d est_noise)
+    Eigen::Matrix3d predictCov(const Eigen::Matrix3d jacob, const Eigen::Matrix3d cov, const Eigen::Matrix3d est_noise)
     {
         auto t_jacob = jacob.transpose();
         auto jacob_cov = jacob * cov;
@@ -92,7 +109,7 @@ namespace ioo_ros2
         return new_cov;
     }
 
-    Eigen::Vector2d update_residual(const Eigen::Vector2d obs, const Eigen::Vector3d est)
+    Eigen::Vector2d updateResidual(const Eigen::Vector2d obs, const Eigen::Vector3d est)
     {
         Eigen::Vector2d result;
         auto h_ = h();
@@ -102,5 +119,88 @@ namespace ioo_ros2
         result(1) = obs.y() - h_est.y();
 
         return result;
+    }
+
+    Eigen::Matrix2d updateS(const Eigen::Matrix3d cov_, const Eigen::Matrix2d obs_noise)
+    {
+        auto convert_cov_ = h().transpose() * cov_ * h();
+
+        return obs_noise + convert_cov_;
+    }
+
+    Eigen::Matrix<double, 3, 2> updateKalmanGain(const Eigen::Matrix2d s, const Eigen::Matrix3d cov)
+    {
+        auto h_ = h().transpose();
+
+        auto inverse_s = s.inverse();
+
+        auto cov_and_h = cov * h_;
+
+        return cov_and_h * inverse_s;
+    }
+
+    Eigen::Vector3d updateX(const Eigen::Vector3d est, const Eigen::Matrix<double, 3, 2> kalman_gain_, const Eigen::Vector2d residual)
+    {
+        auto kalman_res = kalman_gain_ * residual;
+
+        Eigen::Vector3d result;
+        result.setZero();
+
+        result(0) = est.x() + kalman_res.x();
+        result(1) = est.y() + kalman_res.y();
+        result(2) = est.z() + kalman_res.z();
+    }
+
+    Eigen::Matrix3d updateCov(const Eigen::Matrix<double, 3, 2> kalman_gain, const Eigen::Matrix3d cov)
+    {
+        Eigen::Matrix3d i;
+        i.setIdentity();
+
+        auto h_ = h().transpose();
+
+        auto kalman_h = kalman_gain * h_;
+
+        auto i_k_h = i - kalman_h;
+
+        return i_k_h * cov;
+    }
+
+    Eigen::Vector2d obsModel(const Eigen::Vector3d linear_accel)
+    {
+        Eigen::Vector2d model;
+
+        if(linear_accel.z() == 0.0)
+        {
+            if(linear_accel.y() > 0.0)
+            {
+                model(0) = acos(-1.0) / 2.0;
+            }
+            else
+            {
+                model(0) = -1.0 * acos(-1.0) / 2.0;
+            }
+        }
+        else
+        {
+            model(0) = atan(linear_accel.y() / linear_accel.z());
+        }
+
+        if(sqrt(linear_accel.y()*linear_accel.y() + linear_accel.z()*linear_accel.z()) == 0.0)
+        {
+            if(-1.0*linear_accel.x() > 0.0)
+            {
+                model(1) = acos(-1.0) / 2.0;
+            }
+            else
+            {
+                model(1) = -1.0*acos(-1.0) / 2.0;
+            }
+        }
+        else
+        {
+            model(1) = (-1.0 * linear_accel.x()) / atan(sqrt(linear_accel.y()*linear_accel.y() + linear_accel.z()*linear_accel.z()));
+        }
+
+        return model;
     }
 }
