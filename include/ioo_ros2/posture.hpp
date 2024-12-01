b1936e63b21718b5ae746ef5f00b4de170ed042f
motii8128 <default270robo@gmail.com>
2024-12-02 01:42:58 +0900

    up

--- include/ioo_ros2/posture.hpp
+++ include/ioo_ros2/posture.hpp
@@ -19,30 +19,6 @@ namespace ioo_ros2
         Eigen::MatrixXd kalman_gain_;
         Eigen::Vector3d estimation_;
     };
-
-    Eigen::Vector3d getEigenVec3(double x, double y, double z);
-
-    Eigen::Vector3d getInputMatrix(double angular_x, double angular_y, double angular_z);
-
-    Eigen::Matrix<double, 3, 2> h();
-
-    Eigen::Matrix3d jacob(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation);
-
-    Eigen::Vector3d predictX(const Eigen::Vector3d input_matrix, const Eigen::Vector3d estimation);
-
-    Eigen::Matrix3d predictCov(const Eigen::Matrix3d jacob, const Eigen::Matrix3d cov, const Eigen::Matrix3d est_noise);
-
-    Eigen::Vector2d updateResidual(const Eigen::Vector2d obs, const Eigen::Vector3d est);
-
-    Eigen::Matrix2d updateS(const Eigen::Matrix3d cov_, const Eigen::Matrix2d obs_noise);
-
-    Eigen::Matrix<double, 3, 2> updateKalmanGain(const Eigen::Matrix2d s, const Eigen::Matrix3d cov);
-
-    Eigen::Vector3d updateX(const Eigen::Vector3d est, const Eigen::Matrix<double, 3, 2> kalman_gain_, const Eigen::Vector2d residual);
-
-    Eigen::Matrix3d updateCov(const Eigen::Matrix<double, 3, 2> kalman_gain, const Eigen::Matrix3d cov);
-
-    Eigen::Vector2d obsModel(const Eigen::Vector3d linear_accel);
 }
 
 #endif
